2418d176bfe37e9ff9e7c68d7496783ed6e67179
Admin-Komputer\Admin <ehe241@gmail.com>
2019-02-15 12:07:05 +0100

    Added CategoryDTO

--- back/src/main/java/com/crud/books/models/Book.java
+++ back/src/main/java/com/crud/books/models/Book.java
@@ -22,5 +22,51 @@ public class Book {
     @ManyToOne
     private Category category;
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(Integer numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
