b19b9a9e2dbea90fa88ec244dcdec4a86731b3db
Admin-Komputer\Admin <ehe241@gmail.com>
2019-02-15 12:10:07 +0100

    Added BookDTO

---
+++ back/src/main/java/com/crud/books/dtos/BookDTO.java
@@ -0,0 +1,63 @@
+package com.crud.books.dtos;
+
+public class BookDTO {
+    private Integer id;
+
+    private String title;
+
+    private String description;
+
+    private Integer numberOfPages;
+
+    private String authorName;
+
+    private String categoryName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(Integer numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
