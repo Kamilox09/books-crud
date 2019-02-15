37858bfef4b8525f1a47d3abd4cb30ace37d4184
Admin-Komputer\Admin <ehe241@gmail.com>
2019-02-15 13:58:29 +0100

    Added Mapper class

---
+++ back/src/main/java/com/crud/books/controllers/Mapper.java
@@ -0,0 +1,57 @@
+package com.crud.books.controllers;
+
+import com.crud.books.dtos.BookDTO;
+import com.crud.books.dtos.CategoryDTO;
+import com.crud.books.models.Author;
+import com.crud.books.models.Book;
+import com.crud.books.models.Category;
+
+public class Mapper {
+
+    static BookDTO mapToBookDTO(Book book) {
+        BookDTO dto = new BookDTO();
+        dto.setId(book.getId());
+        dto.setTitle(book.getTitle());
+        dto.setDescription(book.getDescription());
+        dto.setNumberOfPages(book.getNumberOfPages());
+        dto.setAuthorName(book.getAuthor().getFullName());
+        dto.setCategoryName(book.getCategory().getName());
+        return dto;
+    }
+
+    static CategoryDTO mapToCategoryDTO(Category category) {
+        CategoryDTO dto = new CategoryDTO();
+        dto.setId(category.getId());
+        dto.setName(category.getName());
+        return dto;
+    }
+
+    static Category mapToCategoryEntity(CategoryDTO dto) {
+        Category entity = new Category();
+        entity.setId(dto.getId());
+        entity.setName(dto.getName());
+        return entity;
+    }
+
+    static Book mapToBookEntity(BookDTO dto){
+        Book entity = new Book();
+        entity.setId(dto.getId());
+        entity.setTitle(dto.getTitle());
+        entity.setDescription(dto.getDescription());
+        entity.setNumberOfPages(dto.getNumberOfPages());
+
+        Author author = new Author();
+        author.setId(null);
+        author.setFullName(dto.getAuthorName());
+
+        Category category = new Category();
+        category.setId(null);
+        category.setName(dto.getCategoryName());
+
+        entity.setAuthor(author);
+        entity.setCategory(category);
+        return entity;
+    }
+
+
+}
